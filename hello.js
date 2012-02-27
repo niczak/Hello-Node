0b722c16a21e60f516c17aee4e2b1b04965ef3fc
Nicholas Kreidberg <niczak@gmail.com>
2012-02-27 10:25:48 -0800

    updated output message

--- hello.js
+++ hello.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) 
 {
-  response.send('Hello World!');
+  response.send('Hello World from PNNode!');
 });
 
 var port = process.env.PORT || 3000;
